4f56ffd0544949474c8e67da464f57350be1a4c9
Joel <jchinop@unsa.edu.pe>
2024-08-22 16:03:28 -0500

    rotador de links

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react';
+import './App.css';
+
+const numbers = [
+  '923404271',
+  '968456626'
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const arraySize = numbers.length;
+    
+    const index = Math.floor((seconds / 2) % arraySize); //El tres representa el intervalo de cambio en seg
+    const message = encodeURIComponent("Hola, me pueden ayudar en ");
+    const whatsappLink = `https://wa.me/51${numbers[index]}?text=${message}`;
+
+    window.location.replace(whatsappLink);
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hola Mundo</h1>
+      <h1>Rediccionanding...</h1>
       <p className="read-the-docs">
         @Todos los derechos reservados
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
